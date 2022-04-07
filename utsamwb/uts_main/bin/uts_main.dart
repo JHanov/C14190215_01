546396d500f87631dc99cf592753dcefcd367fbd
JHanov <91719864+JHanov@users.noreply.github.com>
2022-04-07 13:36:33 +0700

    Add files via upload

---
+++ utsamwb/uts_main/bin/uts_main.dart
@@ -0,0 +1,5 @@
+import 'package:uts_main/uts_main.dart' as uts_main;
+
+void main(List<String> arguments) {
+  print('Hello world: ${uts_main.calculate()}!');
+}
---
+++ utsamwb/uts_main/test/uts_main_test.dart
@@ -0,0 +1,8 @@
+import 'package:uts_main/uts_main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
